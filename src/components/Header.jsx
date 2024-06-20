86eddb1cd51b9c5d08a22ffdd0e7f7d86b1e999d
Paul Savage <paulsavage@Pauls-iMac-3.local>
2024-06-20 13:52:43 +0100

    deploy to Netlify

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { UserContext } from "../contexts/UserContext";
 const Header = () => {
   const { user, setUser } = useContext(UserContext);
 
-  return <h1>NC News - (user: {user})</h1>;
+  return <h1>NC News! - (user: {user})</h1>;
 };
 
 export default Header;
